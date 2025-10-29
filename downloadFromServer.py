af070f521fbf393b74c7c4a39a8d3a30c22faa6f
appzgames03 <88479160+appzgames03@users.noreply.github.com>
2025-10-29 09:29:12 +0530

    Replace hardcoded file path with placeholder
    Updated hardcoded file path to a placeholder.

--- downloadFromServer.py
+++ downloadFromServer.py
@@ -3,7 +3,7 @@ from flask import Flask, send_file
 app = Flask(__name__)
 
 # Hardcoded file path
-file_path = "War 2 (2025) [1080p] [WEBRip] [x265] [10bit] [5.1] [YTS.MX]/War.2.2025.1080p.WEBRip.x265.10bit.AAC5.1-[YTS.MX].mp4"  # replace with your file
+file_path = "<file path>"  # replace with your file
 
 @app.route('/')
 def download():
